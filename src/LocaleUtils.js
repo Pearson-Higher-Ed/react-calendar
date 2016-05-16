fad3e36977daecb25b43029e63f86b45a644c342
gayan <gayan.kumara@pearson.com>
2016-05-16 13:19:19 +0530

    remove unwanted files, change day name as M, T ..

--- src/LocaleUtils.js
+++ src/LocaleUtils.js
@@ -1,8 +1,8 @@
 const WEEKDAYS_LONG = ["Sunday", "Monday", "Tuesday",
   "Wednesday", "Thursday", "Friday", "Saturday"];
 
-const WEEKDAYS_SHORT = ["Su", "Mo", "Tu",
-  "We", "Th", "Fr", "Sa"];
+const WEEKDAYS_SHORT = ["S", "M", "T",
+  "W", "T", "F", "S"];
 
 const MONTHS = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"];
